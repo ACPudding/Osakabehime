383f144014e2be7458016deccd148c7b06f09493
ACPudding <miaomiao2130@gmail.com>
2021-11-14 02:57:38 +0800

    基本功能完成,检查更新尚未移植开启.

--- Properties/CommonStrings.cs
+++ Properties/CommonStrings.cs
@@ -4,8 +4,8 @@ namespace Osakabehime.Properties
 {
     class CommonStrings
     {
-        public static string Version = "版本号:  v0.5.0_b";
-        public static string VersionTag = "v0.5.0_b";
+        public static string Version = "版本号:  v0.9.5_b";
+        public static string VersionTag = "v0.9.5_b";
         public static MessageBoxResult SuperMsgBoxRes;
     }
 }
